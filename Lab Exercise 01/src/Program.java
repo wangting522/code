6cc30ff46f0859c81a1b7daf3adec1effa7bf631
Ting Wang <wangting539@gmail.com>
2023-06-29 14:21:38 -0400

    initial commit

---
+++ Lab Exercise 01/src/Program.java
@@ -0,0 +1,22 @@
+/*
+ * Student Name: Ting Wang
+ * Lab Professor: Wafa Hasanain
+ * Due Date: September 16
+ * Description: Lab Exercise 01 Task 5
+ */
+/* Description: this class outputs several messages
+ * from within method main. */
+public class Program {
+	/* Entry point for the application. */
+	public static void main(String[] args) {
+		String name = "Ting Wang";
+		System.out.println("Welcome to CST8116!");
+		System.out.print("Java says: Hello ");
+		System.out.print(name);
+		System.out.println(); // new-line
+		System.out.println("Program modified by " + name);
+
+
+	}
+
+}
